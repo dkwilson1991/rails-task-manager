5737dd0d12d4feff83fb35c699faa49ee5e9fc04
Keita Wilson <dkwilson1991@gmail.com>
2022-11-08 16:58:48 +0900

    completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,47 @@
 class TasksController < ApplicationController
-  def add_task
+  def index
+    @tasks = Task.all
   end
 
-  def list_all
+  def show
+    @task = Task.find(params[:id])
   end
 
-  def details
+  def new
+    @task = Task.new
   end
 
-  def update_task
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit_task
+  def edit
+    @task = Task.find(params[:id])
   end
 
-  def remove_task
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  root to: "tasks#list"
-  get "task", to: "pages#task"
+  # CRUD
+  # Read all
+  get "/tasks", to: "tasks#index", as: :tasks
+
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
+
+  # Read one
+  get "/tasks/:id", to: "tasks#show", as: :task
+
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+
+  delete "/tasks/:id", to: "tasks#destroy"
 end
